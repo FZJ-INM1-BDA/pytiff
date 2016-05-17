7849c3728ef7c952e39b10a56850db770a7441e5
Philipp Glock <phil.glock@gmail.com>
2016-05-17 17:18:18 +0200

    fixed imports

--- test/test_tiff.py
+++ test/test_tiff.py
@@ -1,9 +1,8 @@
 import pytiff
 
+DATA_DIR = ""
 TILED_GREY = "test_data/small_example_tiled.tif"
 
 def test_open():
-    from pytiff import _version
-    print(_version.__file__)
     tif = pytiff.Tiff(TILED_GREY)
     assert True
