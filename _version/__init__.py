afce841bfc26f1b471e93a294dbe05758a37edb3
Philipp Glock <phil.glock@gmail.com>
2016-05-17 15:56:41 +0200

    added module for version

---
+++ _version/__init__.py
@@ -0,0 +1,20 @@
+"""
+_version
+Version information for pytiff.
+"""
+
+__all__ = [
+    'VERSION_MAJOR',
+    'VERSION_MINOR',
+    'VERSION_PATCH',
+    'VERSION_STRING',
+    '__version__',
+]
+
+VERSION_MAJOR = 0
+VERSION_MINOR = 3
+VERSION_PATCH = 2
+
+VERSION_STRING = "{major}.{minor}.{patch}".format(major=VERSION_MAJOR, minor=VERSION_MINOR, patch=VERSION_PATCH)
+
+__version__ = VERSION_STRING
