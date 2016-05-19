f3bf1b11c90f6e7ec5f8527406be5f197dfc908e
Philipp Glock <phil.glock@gmail.com>
2016-05-19 10:47:41 +0200

    implemented rgb tests

--- test/test_tiff.py
+++ test/test_tiff.py
@@ -4,6 +4,9 @@ import tifffile
 import numpy as np
 
 TILED_GREY = "test_data/small_example_tiled.tif"
+NOT_TILED_GREY = "test_data/small_example.tif"
+TILED_RGB = "test_data/tiled_rgb_sample.tif"
+NOT_TILED_RGB = "test_data/rgb_sample.tif"
 NO_FILE = "test_data/not_here.tif"
 
 def test_open():
@@ -15,11 +18,24 @@ def test_open_fail():
         tif = pytiff.Tiff(NO_FILE)
 
 def test_greyscale_tiled():
-    with tifffile.TiffFile(TILED_GREY) as tif:
+    read_methods(TILED_GREY)
+
+
+def test_greyscale_not_tiled():
+    read_methods(NOT_TILED_GREY)
+
+def test_rgb_tiled():
+    read_methods(TILED_RGB)
+
+def test_rgb_not_tiled():
+    read_methods(NOT_TILED_RGB)
+
+def read_methods(filename):
+    with tifffile.TiffFile(filename) as tif:
         for page in tif:
             first_page = page.asarray()
 
-    with pytiff.Tiff(TILED_GREY) as tif:
+    with pytiff.Tiff(filename) as tif:
         data = tif[:]
         # test reading whole page
         np.testing.assert_array_equal(first_page, data)
@@ -32,13 +48,3 @@ def test_greyscale_tiled():
 
         chunk = tif[100:200, 250:350]
         np.testing.assert_array_equal(first_page[100:200, 250:350], chunk)
-
-
-def test_greyscale_not_tiled():
-    assert False
-
-def test_rgb_tiled():
-    assert False
-
-def test_rgb_not_tiled():
-    assert False
