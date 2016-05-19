490f77fd403c0f2b205510e3cc920324e604dd41
Philipp Glock <phil.glock@gmail.com>
2016-05-19 11:10:47 +0200

    basic reading tests for grey and rgb

--- test/test_tiff.py
+++ test/test_tiff.py
@@ -20,27 +20,63 @@ def test_open_fail():
 def test_greyscale_tiled():
     read_methods(TILED_GREY)
 
-
 def test_greyscale_not_tiled():
     read_methods(NOT_TILED_GREY)
 
 def test_rgb_tiled():
-    read_methods(TILED_RGB)
+    with tifffile.TiffFile(TILED_RGB) as tif:
+        for page in tif:
+            first_page = page.asarray()
+            break
+
+    with pytiff.Tiff(TILED_RGB) as tif:
+        data = tif[:]
+        # test reading whole page
+        np.testing.assert_array_equal(first_page, data)
+        # test reading a chunk
+        chunk = tif[100:200, :, :3]
+        np.testing.assert_array_equal(first_page[100:200, :], chunk)
+
+        chunk = tif[:, 250:350]
+        np.testing.assert_array_equal(first_page[:, 250:350], chunk)
+
+        chunk = tif[100:200, 250:350]
+        np.testing.assert_array_equal(first_page[100:200, 250:350], chunk)
 
 def test_rgb_not_tiled():
-    read_methods(NOT_TILED_RGB)
+    with tifffile.TiffFile(NOT_TILED_RGB) as tif:
+        for page in tif:
+            first_page = page.asarray()
+            break
+        alpha = np.ones_like(first_page[:,:,0]) * 255
+        first_page = np.dstack((first_page, alpha))
+
+    with pytiff.Tiff(NOT_TILED_RGB) as tif:
+        data = tif[:]
+        # test reading whole page
+        np.testing.assert_array_equal(first_page, data)
+        # test reading a chunk
+        chunk = tif[100:200, :]
+        np.testing.assert_array_equal(first_page[100:200, :], chunk)
+
+        chunk = tif[:, 250:350]
+        np.testing.assert_array_equal(first_page[:, 250:350], chunk)
+
+        chunk = tif[100:200, 250:350]
+        np.testing.assert_array_equal(first_page[100:200, 250:350], chunk)
 
 def read_methods(filename):
     with tifffile.TiffFile(filename) as tif:
         for page in tif:
             first_page = page.asarray()
+            break
 
     with pytiff.Tiff(filename) as tif:
         data = tif[:]
         # test reading whole page
         np.testing.assert_array_equal(first_page, data)
         # test reading a chunk
-        chunk = tif[100:200, :]
+        chunk = tif[100:200, :, :3]
         np.testing.assert_array_equal(first_page[100:200, :], chunk)
 
         chunk = tif[:, 250:350]
