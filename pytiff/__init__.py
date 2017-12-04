32902066efaa9fb02d0b242f5744d985c46681c6
Philipp Glock <phil.glock@gmail.com>
2017-12-04 14:15:34 +0100

    appveyor only build master, __init__.py only catches ModuleNotFoundError

--- pytiff/__init__.py
+++ pytiff/__init__.py
@@ -5,6 +5,7 @@ try:
     from ._pytiff import Tiff
     from ._pytiff import __doc__
     from ._pytiff import tiff_version, tiff_version_raw
-except ImportError as e:
+except ModuleNotFoundError as e:
     print("Cython modules not available")
+
 from pytiff._version import __version__
