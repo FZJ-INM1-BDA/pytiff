aafeabddf4697a0e0fb26208af10a8c61952d26f
Philipp Glock <pglock@users.noreply.github.com>
2017-12-04 15:25:25 +0100

    Catch import error

--- pytiff/__init__.py
+++ pytiff/__init__.py
@@ -5,7 +5,7 @@ try:
     from ._pytiff import Tiff
     from ._pytiff import __doc__
     from ._pytiff import tiff_version, tiff_version_raw
-except ModuleNotFoundError as e:
+except ImportError as e:
     print("Cython modules not available")
 
 from pytiff._version import __version__
